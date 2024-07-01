2eadba5860aa2a5ee8d3da892f95137d92b9a858
vanvixi <vanvixi.dev@gmail.com>
2024-07-02 01:23:36 +0700

    📝 (IProductDao.java): Add add and findAll methods to IProductDao interface

--- src/main/java/com/rxmobileteam/lecture1/service/IProductDao.java
+++ src/main/java/com/rxmobileteam/lecture1/service/IProductDao.java
@@ -1,2 +1,12 @@
-package com.rxmobileteam.lecture1.service;public interface IProductDao {
+package com.rxmobileteam.lecture1.service;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+public interface IProductDao {
+    boolean add(@NotNull Product product);
+
+
+    Set<Product> findAll();
 }
