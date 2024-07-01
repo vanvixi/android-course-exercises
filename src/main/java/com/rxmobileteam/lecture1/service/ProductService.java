aa0c6e61f9c3afe94f5362e60ea37473a2e7b5d0
vanvixi <vanvixi.dev@gmail.com>
2024-07-02 01:23:41 +0700

    📝 (ProductService.java): Implement addProduct and searchProducts methods in ProductService class

--- src/main/java/com/rxmobileteam/lecture1/service/ProductService.java
+++ src/main/java/com/rxmobileteam/lecture1/service/ProductService.java
@@ -4,7 +4,9 @@ import com.rxmobileteam.lecture1.data.ProductDao;
 import com.rxmobileteam.utils.ExerciseNotCompletedException;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * {@link ProductService} provides an API that allows to manage {@link Product}s.
@@ -14,6 +16,13 @@ import java.util.List;
  */
 public class ProductService {
 
+    public ProductService(IProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    final IProductDao productDao;
+
+
     /**
      * Adds a new product to the system.
      *
@@ -21,8 +30,7 @@ public class ProductService {
      * @return {@code true} if a product was added, {@code false} otherwise.
      */
     public boolean addProduct(@NotNull Product product) {
-        // TODO: implement this method
-        throw new ExerciseNotCompletedException();
+        return productDao.add(product);
     }
 
     /**
@@ -32,7 +40,6 @@ public class ProductService {
      * @return a list of found products
      */
     public List<Product> searchProducts(String query) {
-        // TODO: implement this method
-        throw new ExerciseNotCompletedException();
+        return new ArrayList<>(productDao.findAll());
     }
 }
