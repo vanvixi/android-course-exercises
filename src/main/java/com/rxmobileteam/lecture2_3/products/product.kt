d4524479dbdae1926f80627662d3867a7d621270
vanvixi <vanvixi.dev@gmail.com>
2024-07-22 14:36:55 +0700

    📝 Implement functions to sort, filter, and manipulate lists and sets of products and orders

--- src/main/java/com/rxmobileteam/lecture2_3/products/product.kt
+++ src/main/java/com/rxmobileteam/lecture2_3/products/product.kt
@@ -1,7 +1,5 @@
 package com.rxmobileteam.lecture2_3.products
 
-import com.rxmobileteam.utils.ExerciseNotCompletedException
-
 enum class ProductCategory {
   LAPTOP,
   PHONE,
@@ -24,36 +22,36 @@ data class Order(
   val isDelivered: Boolean,
 )
 
-// TODO: Return a list of Product, sorted in the ascending by price. if prices are equal, sorted by favoriteCount descending
-fun List<Product>.sortedByPriceAscendingThenByFavoriteCountDescending(): List<Product> =
-  throw ExerciseNotCompletedException()
+// Return a list of Product, sorted in the ascending by price. if prices are equal, sorted by favoriteCount descending
+fun List<Product>.sortedByPriceAscendingThenByFavoriteCountDescending(): List<Product> {
+  return this.sortedWith(compareBy<Product> { it.price }.thenByDescending { it.favoriteCount })
+}
 
-// TODO: Return a set of Products in the orders (The order doesn't matter).
-fun List<Order>.getProductsSet(): Set<Product> = throw ExerciseNotCompletedException()
+//  Return a list of Products in the orders, duplicates are allowed.
+fun List<Order>.getProductsList(): List<Product> = this.flatMap { it.products }
 
-// TODO: Return a list of Products in the orders, duplicates are allowed.
-fun List<Order>.getProductsList(): List<Product> = throw ExerciseNotCompletedException()
+//  Return a set of Products in the orders (The order doesn't matter).
+fun List<Order>.getProductsSet(): Set<Product> = this.getProductsList().toSet()
 
-// TODO: Return a list of delivered orders
-fun List<Order>.getDeliveredOrders(): List<Order> = throw ExerciseNotCompletedException()
+//  Return a list of delivered orders
+fun List<Order>.getDeliveredOrders(): List<Order> = this.filter { it.isDelivered }
 
-// TODO: Return a list of products in the delivered orders
-fun List<Order>.getDeliveredProductsList(): List<Product> = throw ExerciseNotCompletedException()
+//  Return a list of products in the delivered orders
+fun List<Order>.getDeliveredProductsList(): List<Product> = this.getDeliveredOrders().flatMap { it.products }
 
-// TODO: Partition the orders into two lists: "delivered" and "not delivered"
-fun List<Order>.partitionDeliveredAndNotDelivered(): Pair<List<Order>, List<Order>> =
-  throw ExerciseNotCompletedException()
+//  Partition the orders into two lists: "delivered" and "not delivered"
+fun List<Order>.partitionDeliveredAndNotDelivered(): Pair<List<Order>, List<Order>> = this.partition { it.isDelivered }
 
-// TODO: Return a map of product to count of this product in the orders
-// eg. [Product1 -> 2, Product2 -> 1, Product3 -> 3]
-fun List<Order>.countOfEachProduct(): Map<Product, Int> = throw ExerciseNotCompletedException()
+//  Return a map of product to count of this product in the orders
+//  Eg: [Product1 -> 2, Product2 -> 1, Product3 -> 3]
+fun List<Order>.countOfEachProduct(): Map<Product, Int> = this.getProductsList().groupingBy { it }.eachCount()
 
-// TODO: Return the sum of product prices in the order
-fun Order.sumProductPrice(): Double = throw ExerciseNotCompletedException()
+//  Return the sum of product prices in the order
+fun Order.sumProductPrice(): Double = this.products.sumOf { it.price }
 
-// TODO: Return the product with the maximum price in the order
-fun Order.getMaxPriceProduct(): Product = throw ExerciseNotCompletedException()
+//  Return the product with the maximum price in the order
+fun Order.getMaxPriceProduct(): Product = this.products.maxWith(compareBy { it.price })
 
-// TODO: Return the product with the min price in the order
-fun Order.getMinPriceProduct(): Product = throw ExerciseNotCompletedException()
+//  Return the product with the min price in the order
+fun Order.getMinPriceProduct(): Product = this.products.minWith(compareBy { it.price })
 
